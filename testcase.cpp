dad5fc448acf733836489210a0ebc97f15f7b602
Ravikant Yadav <101169995+Ravikant3243@users.noreply.github.com>
2022-10-26 19:42:44 +0530

    Update testcase.cpp

--- testcase.cpp
+++ testcase.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
-#define fo(i,n) for(int i=0;i<n;i++)
+#include <bits/stdc++.h>
+#define fo(i, n) for (int i = 0; i < n; i++)
 #define ll long long
-#define mod 1e9+7
-#define eb(x) emplace_back(x) 
-#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define mod (int(1e9 + 7))
+
+#define eb(x) emplace_back(x)
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL)
 #define endl "\n"
 using namespace std;
 
-int main(){
-  int t;
-  cin>>t;
-  while(t--){
-     
-  }
-return 0;
+int main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+   }
+   return 0;
 }
